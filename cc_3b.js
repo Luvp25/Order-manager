f79d00a49bf555fd04e815c298018a59da5d9027
Luv Patel <luvp98@gmail.com>
2026-02-08 02:23:36 -0500

    Figured out how to use forEach

---
+++ cc_3b.js
@@ -0,0 +1,34 @@
+let inventory = [
+    {
+        Sku: "SKU-002",
+        Name: "Owala",
+        Price: 40,
+        Stock: 5
+
+    },
+
+    {
+        Sku: "SKU-003",
+        Name: "Stanley",
+        Price: 35,
+        Stock: 10
+    },
+
+    {
+        Sku: "SKU-004",
+        Name: "Larq",
+        Price: 100,
+        Stock: 13
+    },
+
+    {
+        Sku: "SKU-005",
+        Name: "Hydro Jug",
+        Price: 45,
+        Stock: 5
+    }
+];
+
+inventory.forEach(function(prod) {
+    console.log(`${prod.Sku} | ${prod.Name} | $${prod.Price} | ${prod.Stock}`)
+});
